227fc43259128ac9d6139df83bd9bb4bc5654618
Andsonyz <antonioandson1307@gmail.com>
2025-04-25 10:34:29 -0300

    Questao2: Implementacao do int main e finalizacao da questao.

--- QuestoesIniciais/Questao2/Q2.C
+++ QuestoesIniciais/Questao2/Q2.C
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int pot(int base, int expoente){
+int poten(int base, int expoente){
     int i;
     int resultado = 1;
     for (i = 0; i < expoente; i++){
@@ -8,4 +8,15 @@ int pot(int base, int expoente){
     }
 
     return resultado;
+}
+
+int main (){
+    int base, expoente;
+    printf("Digite a base: ");
+    scanf("%d", &base);
+    printf("Digite o expoente: ");
+    scanf("%d", &expoente);
+
+    printf("%d^%d = %d\n", base, expoente, poten(base, expoente));
+    return 0;
 }
